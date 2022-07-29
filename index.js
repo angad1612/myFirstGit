826a307d75ffa56bc485bf4cda5dc78307b18118
angadkler <103878484+angad1612@users.noreply.github.com>
2022-07-29 10:18:24 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 //CREATE A NEW NODE PROJECT THAT PRINTS OUT "HELLO WORLD"
 
 console.log("Hello World")
-
-
-//FUNCTION WHICH ADDS THE NUMBERS 5 AND 10
-function addnums(num1, num2){
-  var result = num1 + num2;
-  return result;
-}
-
-console.log(addnums(5, 10));
