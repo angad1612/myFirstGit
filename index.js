b134a66c7cebbd09e14938ff27b86f41910b5665
Angad <angad.kler1@gmail.com>
2022-07-29 10:39:30 +1000

    added adding function

---
+++ index.js
@@ -0,0 +1,12 @@
+//CREATE A NEW NODE PROJECT THAT PRINTS OUT "HELLO WORLD"
+
+console.log("Hello World")
+
+//FUNCTION WHICH ADDS THE NUMBERS 5 AND 10
+function addnums(num1, num2){
+    var result = num1 + num2;
+    return result;
+  }
+  
+  console.log(addnums(5, 10));
+  
