7a282bdbedfff5d076416a0dd5193ffe9fa3d689
Angad <angad.kler1@gmail.com>
2022-07-29 10:24:40 +1000

    added hello world

--- index.js
+++ index.js
@@ -3,10 +3,3 @@
 console.log("Hello World")
 
 
-//FUNCTION WHICH ADDS THE NUMBERS 5 AND 10
-function addnums(num1, num2){
-  var result = num1 + num2;
-  return result;
-}
-
-console.log(addnums(5, 10));
